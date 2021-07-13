b3aa8f3d575392d63b2a0a96205bba09cdb92376
Vesel4ak <68870977+Vesel4akRepositories@users.noreply.github.com>
2021-07-13 16:13:31 +0300

    first commit

---
+++ recyclerview/build/generated/source/buildConfig/debug/android/support/v7/recyclerview/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package android.support.v7.recyclerview;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "android.support.v7.recyclerview";
+  public static final String BUILD_TYPE = "debug";
+}
